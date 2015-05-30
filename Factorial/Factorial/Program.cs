60e2700c3a9e900277d4843996be50f4a5f339db
ltdansby86 <ltdansby86@yahoo.com>
2015-05-29 21:33:07 -0400

    second test commit

--- Factorial/Factorial/Program.cs
+++ Factorial/Factorial/Program.cs
@@ -17,13 +17,14 @@ namespace Factorial
 
        static void Factorial(int fac)
         {
-            int result = 1;
+            
            if (fac == 0)
            {
                Console.WriteLine("1");
            }
            else
            {
+               int result = 1;
                while( fac > 0)
                {
                    result = result * fac;
